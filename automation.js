23dfa24cb834bc5baca806549d3960ff6230b79e
QubazWeb <qubazdev@gmail.com>
2025-11-20 17:44:07 +0100

    added package.json

--- automation.js
+++ automation.js
@@ -84,4 +84,5 @@ app.post("/notion-hook", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
